0d4a5c88fa1decde9f9fa2710bef279df7b21351
clefersondannylo <clefersonnascimento8@gmail.com>
2022-01-23 21:44:15 -0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+
+export function Home(){
+  return(
+    <>
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Text style={{color:'#000'}}>
+          React-Native
+        </Text>
+      </View>
+    </>
+        )
+}
